cf9118c9cbe46f2c54f9142b791e2daccf2314a6
GreenSurge <124407395+GreenSurge@users.noreply.github.com>
2026-01-21 23:33:41 -0600

    Add configuration for Supabase URL and keys

---
+++ js/config.js
@@ -0,0 +1,3 @@
+export const SUPABASE_URL = "https://jlaplirgtsjkyokyaspd.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpsYXBsaXJndHNqa3lva3lhc3BkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkwNTAwMDgsImV4cCI6MjA4NDYyNjAwOH0.32K6jeh1VWrUPoHNdw6aLWnt2r0_2Al_7tyYBFwHF88";
+export const REDIRECT_TO = `${location.origin}/app.html`;
